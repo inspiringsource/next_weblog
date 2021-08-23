6c052d1be2a670c7b66c5674d1e75edf606b6d0c
Abe <ab.b2k3@gmail.com>
2021-08-23 17:55:30 +0200

    initial commit

---
+++ pages/[username]/[slug].js
@@ -0,0 +1,24 @@
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  return (
+    <main className={styles.container}>
+
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+
+      </aside>
+    </main>
+  );
+}
---
+++ pages/admin/[slug].js
@@ -0,0 +1,11 @@
+import Metatags from "../../components/Metatags"
+
+export default function AdminPostPage({}) {
+    return (
+        <main>
+            <Metatags title="admin page"/>
+            <h1>Edit Post</h1>
+        </main>
+    )
+
+}
