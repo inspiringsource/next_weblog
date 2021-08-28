dd005ba81af8fd5df9e1ac8a3eca6f93579cb455
Abe <ab.b2k3@gmail.com>
2021-08-28 10:58:11 +0200

    full working demo

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,21 +1,21 @@
-import styles from '../../styles/Admin.module.css';
-import AuthCheck from '../../components/AuthCheck';
-import { firestore, auth, serverTimestamp } from '../../lib/firebase';
-import ImageUploader from '../../components/ImageUploader';
+import styles from '../../styles/Admin.module.css'
+import AuthCheck from '../../components/AuthCheck'
+import { firestore, auth, serverTimestamp } from '../../lib/firebase'
+import ImageUploader from '../../components/ImageUploader'
 
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import { useForm } from 'react-hook-form';
-import ReactMarkdown from 'react-markdown';
-import Link from 'next/link';
-import toast from 'react-hot-toast';
+import { useDocumentDataOnce } from 'react-firebase-hooks/firestore'
+import { useForm } from 'react-hook-form'
+import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
+import toast from 'react-hot-toast'
 
 export default function AdminPostEdit(props) {
   return (
     <AuthCheck>
-        <PostManager />
+      <PostManager />
     </AuthCheck>
   );
 }
@@ -27,7 +27,7 @@ function PostManager() {
   const { slug } = router.query;
 
   const postRef = firestore.collection('users').doc(auth.currentUser.uid).collection('posts').doc(slug);
-  const [post] = useDocumentData(postRef);
+  const [post] = useDocumentDataOnce(postRef);
 
   return (
     <main className={styles.container}>
@@ -41,11 +41,12 @@ function PostManager() {
           </section>
 
           <aside>
-          <h3>Tools</h3>
+            <h3>Tools</h3>
             <button onClick={() => setPreview(!preview)}>{preview ? 'Edit' : 'Preview'}</button>
             <Link href={`/${post.username}/${post.slug}`}>
               <button className="btn-blue">Live view</button>
             </Link>
+            <DeletePostButton postRef={postRef} />
           </aside>
         </>
       )}
@@ -54,9 +55,19 @@ function PostManager() {
 }
 
 function PostForm({ defaultValues, postRef, preview }) {
-    const { register, handleSubmit, reset, watch, formState, errors } = useForm({ defaultValues, mode: 'onChange' });
+  const {
+    register,
+    handleSubmit,
+    formState,
+    reset,
+    watch,
+  } = useForm({ defaultValues, mode: 'onChange' });
 
-    const { isValid, isDirty } = formState;
+  const {
+    errors,
+  } = formState;
+
+  const { isValid, isDirty } = formState;
 
   const updatePost = async ({ content, published }) => {
     await postRef.update({
@@ -67,7 +78,7 @@ function PostForm({ defaultValues, postRef, preview }) {
 
     reset({ content, published });
 
-    toast.success('Post updated successfully!')
+    toast.success('Post updated successfully!');
   };
 
   return (
@@ -79,27 +90,46 @@ function PostForm({ defaultValues, postRef, preview }) {
       )}
 
       <div className={preview ? styles.hidden : styles.controls}>
+        <ImageUploader />
 
-    <ImageUploader />
-    Backend Security
-      <textarea name="content" ref={register({
+        <textarea
+          {...register('content', {
             maxLength: { value: 20000, message: 'content is too long' },
             minLength: { value: 10, message: 'content is too short' },
-            required: { value: true, message: 'content is required'}
-          })}>
-      </textarea>
+            required: { value: true, message: 'content is required' },
+          })}></textarea>
 
         {errors.content && <p className="text-danger">{errors.content.message}</p>}
 
+
         <fieldset>
-          <input className={styles.checkbox} name="published" type="checkbox" ref={register} />
+          <input className={styles.checkbox} {...register('published')} type="checkbox" />
           <label>Published</label>
         </fieldset>
 
-        <button type="submit" disabled={!isDirty || !isValid}>
+        <button type="submit" className="btn-green" disabled={!isDirty || !isValid}>
           Save Changes
         </button>
       </div>
     </form>
+  );
+}
+
+function DeletePostButton({ postRef }) {
+  const router = useRouter();
+
+  const deletePost = async () => {
+    const doIt = confirm('are you sure!');
+    if (doIt) {
+      await postRef.delete();
+      router.push('/admin');
+      toast('post annihilated ', { icon: '🗑️' });
+    }
+  };
+
+  return (
+    <button className="btn-red" onClick={deletePost}>
+      Delete
+    </button>
   );
 }
